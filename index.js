a467ee8083ec0cd7349475ecc214ead5f09afa1f
Tamir Levi <tamir@apiiro.com>
2024-06-20 10:25:42 +0300

    another dep usage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express')
+const react = require('react')
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
